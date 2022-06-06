3aaf77004dd9334e320cca8ad4a2b6f83b8eb42d
BenFaruna <benjaminfaruna@gmail.com>
2022-06-07 00:16:58 +0100

    Added corrections for failed checks

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_num = my_list[0]
-
-    for num in my_list[1:]:
-        if (num > max_num):
-            max_num = num
-
-    return (max_num)
+    if my_list:
+        my_list.sort(reverse=True)
+        return (my_list[0])
+    return (None)
