ec503a8224d698ce499fc6b14c302cec1ff59a2b
BenFaruna <benjaminfaruna@gmail.com>
2022-07-05 02:06:33 +0100

    101-add_attribute.py pep-8

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module to add attribute method"""
 
+
 def add_attribute(obj, name, value):
     """function for adding new attributes to an object"""
     if hasattr(obj, "__dict__"):
