4be65f7859bb4e05a24bab45fc9e6dfda3601d30
BenFaruna <benjaminfaruna@gmail.com>
2022-07-06 02:48:49 +0100

    completed mandatory task for 0x0B-python-input_output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -9,6 +9,14 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """ retrieves a dictionary representation of a Student"""
-        return self.__dict__.copy()
+        if attrs is None:
+            return self.__dict__.copy()
+        return {k: self.__dict__.get(k) for k in attrs
+                if k in self.__dict__.keys()}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            self.__dict__[key] = value
