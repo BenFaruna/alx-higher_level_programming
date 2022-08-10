852c8f6857a398c5d7180f1d260e35d81f5c0191
Benjamin Faruna <benjaminfaruna@gmail.com>
2022-08-10 02:07:52 +0000

    updated 100-move_to_sql to change column name of first_table to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,4 @@
 -- converts first_table table in htbn_0c_0 database character set to utf8
 ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE htbn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+ALTER TABLE htbn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
