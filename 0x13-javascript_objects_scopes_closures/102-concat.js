1a1fd641b8aa2092458add66916d1f9790fec021
BenFaruna <benjaminfaruna@gmail.com>
2022-09-07 06:36:20 +0100

    Completed advanced task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 
-const { readFileSync } = require('fs');
+const fs = require('fs');
 
-function concat (firstFile, secondFile) {
-  const content1 = readFileSync(firstFile);
-  const content2 = readFileSync(secondFile);
+function concat (firstFile, secondFile, saveFile) {
+  const content1 = fs.readFileSync(firstFile);
+  const content2 = fs.readFileSync(secondFile);
 
-  return (content1 + content2);
+  fs.writeFileSync(saveFile, (content1 + content2));
 }
 
 const file1 = process.argv[2];
 const file2 = process.argv[3];
+const file3 = process.argv[4]
 
-if (process.argv.length > 2) {
-  console.log(concat(file1, file2));
+if (process.argv.length === 5) {
+  concat(file1, file2, file3);
 }
