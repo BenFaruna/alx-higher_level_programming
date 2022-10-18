ecc982a6b04679a9ea7d2efe4e487d04660c6341
BenFaruna <benjaminfaruna@gmail.com>
2022-10-18 14:45:16 +0000

    __

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const url = process.argv[2];
-const character = 'https://swapi-api.hbtn.io/api/people/18/';
+const id = '/18/';
 let moviesCount = 0;
 
 request(url, (error, response, body) => {
@@ -13,8 +13,10 @@ request(url, (error, response, body) => {
   }
 
   for (const film of data.results) {
-    if (film.characters.includes(character)) {
-      moviesCount = moviesCount + 1;
+	  for (const character of film.characters) {
+      if (character.endsWith(id)) {
+        moviesCount = moviesCount + 1;
+      }
     }
   }
   console.log(moviesCount);
